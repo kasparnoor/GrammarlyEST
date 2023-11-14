dd8ca4741d0e8e979ae6fe403b50d5e6354bffe3
Kaspar Noor <kaspar.noor@real.edu.ee>
2023-11-14 13:17:02 +0200

    a

--- Degrammarize/repunctuate.py
+++ Degrammarize/repunctuate.py
@@ -1,6 +1,4 @@
-from utils.read_sentences import get_sentences
-def repunctuate(list_of_words:list[str]):
-    return(list_of_words)
-
-sentence = get_sentences()["1"]
-repunctuate(sentence)
+def repunctuate(sentence:str):
+    sentence = sentence.split(" ")
+    sentence = [i for i in sentence if i not in [',', ':', ';', '.','!', '?', '–', '"']] 
+    return(" ".join(sentence))
--- Degrammarize/utils/read_sentences.py
+++ Degrammarize/utils/read_sentences.py
@@ -1,6 +1,6 @@
 import json
-
+import codecs
 def get_sentences():
-    json_file = open("utils/sentences.json", 'r')
-    json_obj = json.loads("\n".join(json_file.readlines()))
+    json_file = codecs.open("utils/sentences.json", "r", "utf-8")
+    json_obj = json.loads("\n".join(json_file.readlines()).encode('utf-8'))
     return(json_obj)
