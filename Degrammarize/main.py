6f41c0ecf2068609602b02ab681aac2fc5f1874b
Kaspar Noor <kaspar.noor@real.edu.ee>
2023-11-14 13:23:45 +0200

    Read sentences fix

--- Degrammarize/main.py
+++ Degrammarize/main.py
@@ -1,7 +1,13 @@
 from utils.read_sentences import get_sentences
-
+from repunctuate import repunctuate
+from morphology import 
 def main():
-    sentence = get_sentences()['1']
-    print(sentence)
+    for sentence in get_sentences().values():
+        # Remove all punctuation
+        sentence_wrong = repunctuate(sentence)
+        # Mess up the forms of words
+        sentence_wrong = 
+        print(sentence, "--->", sentence_wrong)
+        print()
 if __name__ == "__main__":
     main()
