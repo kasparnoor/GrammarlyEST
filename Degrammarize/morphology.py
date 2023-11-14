1b163631c630da896f28fee90b358777ef398b2e
kaur <kaur.ilison@gmail.com>
2023-11-14 13:22:54 +0200

    initial commit

---
+++ Degrammarize/morphology.py
@@ -0,0 +1,10 @@
+from estnltk import Text
+
+from utils.read_sentences import get_sentences
+def repunctuate(list_of_words:list[str]):
+    return(list_of_words)
+
+sentence = Text(get_sentences()["1"])
+#print(repunctuate(sentence))
+sentence.morph_analysis[]
+
