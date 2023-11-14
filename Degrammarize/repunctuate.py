d51d87cf8638c6e858ccbb04888f3349019b7eab
Kaspar Noor <kaspar.noor@real.edu.ee>
2023-11-14 13:00:32 +0200

    Fix

--- Degrammarize/repunctuate.py
+++ Degrammarize/repunctuate.py
@@ -1,9 +1,6 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join('..', 'utils')))
-print(sys.path)
 from utils.read_sentences import get_sentences
 def repunctuate(list_of_words:list[str]):
-    return(list_of_words.remove(','))
+    return(list_of_words)
 
-sentence = get_sentences()[0]
+sentence = get_sentences()["1"]
 repunctuate(sentence)
--- Degrammarize/utils/read_sentences.py
+++ Degrammarize/utils/read_sentences.py
@@ -1,6 +1,6 @@
 import json
 
 def get_sentences():
-    json_file = open("Degrammarize/utils/sentences.json", 'r')
-    json_obj = json.loads(json_file)
+    json_file = open("utils/sentences.json", 'r')
+    json_obj = json.loads("\n".join(json_file.readlines()))
     return(json_obj)
